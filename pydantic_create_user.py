346f05cd3680861dc199df62936a497ba9868c55
DzhN <djanai9219@gmail.com>
2026-03-30 22:34:13 +0300

    Практика написания Pydantic-моделей

--- pydantic_create_user.py
+++ pydantic_create_user.py
@@ -1,16 +1,28 @@
 from pydantic import BaseModel, EmailStr, Field
 
 class ShortUserSchema(BaseModel):
+    """
+    Описание краткой модели данных пользователя
+    """
     email: EmailStr
     last_name:str = Field(alias="lastName")
     first_name: str = Field(alias="firstName")
     middle_name: str = Field(alias="middleName")
 
 class ExtendedUserSchema(ShortUserSchema):
+    """
+    Описание полной модели данных пользователя: краткая модель + id
+    """
     id: str
 
 class CreateUserRequestSchema(ShortUserSchema):
+    """
+    Описание модели данных запроса на создание пользователя
+    """
     password: str
 
 class CreateUserResponseSchema(ExtendedUserSchema):
+    """
+    Описание модели данных ответа на создание пользователя
+    """
     user: ExtendedUserSchema
