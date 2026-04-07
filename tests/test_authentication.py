618d06a01d11acc7531191d356a9a083990c1155
DzhN <djanai9219@gmail.com>
2026-04-07 23:20:17 +0300

    Практика написания API автотестов

---
+++ tests/test_authentication.py
@@ -0,0 +1,27 @@
+from http import HTTPStatus
+
+from httpcore import request
+
+from clients.api_client_create_exercise import authentication_user
+from clients.authentication.authentication_client import get_authentication_client
+from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
+from clients.users.public_users_client import get_public_users_client
+from clients.users.users_schema import CreateUserRequestSchema
+from tools.assertions.authentication import assert_login_response
+from tools.assertions.base import assert_status_code, assert_is_true
+
+
+def test_login():
+    public_user_client = get_public_users_client()
+    create_user_request = CreateUserRequestSchema()
+    public_user_client.create_user(create_user_request)
+    login_request = LoginRequestSchema(
+        email=create_user_request.email,
+        password=create_user_request.password
+    )
+    authentication_client = get_authentication_client()
+    login_response = authentication_client.login_api(login_request)
+    login_response_data = LoginResponseSchema.model_validate_json(login_response.text)
+
+    assert_status_code(login_response.status_code, HTTPStatus.OK)
+    assert_login_response(login_response_data)
